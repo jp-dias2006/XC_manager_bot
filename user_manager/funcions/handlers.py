fa2f887d51dea378aafa29a9030fcc0c78763d5c
jp-dias2006 <pc.jpdl15@gmail.com>
2025-07-04 00:01:52 -0300

    New structure for project

--- user_manager/funcions/handlers.py
+++ user_manager/funcions/handlers.py
@@ -3,3 +3,6 @@ from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Bem-vindo ao grupo VIP!')
+
+async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Gostaria de saber mais sobre o grupo VIP?')
